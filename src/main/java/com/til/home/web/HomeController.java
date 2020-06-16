3293039ea6da356e24efdcf01883621435ca006c
hotsun0428 <hotsun0428@61.74.31.137>
2020-06-17 07:03:02 +0900

    web.xml 초기설정

--- src/main/java/com/til/home/web/HomeController.java
+++ src/main/java/com/til/home/web/HomeController.java
@@ -1,5 +1,14 @@
 package com.til.home.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
 
+	@RequestMapping(value="/home.do")
+	public String home(ModelMap model) throws Exception {
+		return "/home";
+	}
 }
